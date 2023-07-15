162eeb32587b4735b49d7f89df41b615a0077ad0
warrior <warrior22@qq.com>
2023-07-15 16:12:41 +0800

    利用宏重用异常处理代码

--- source/kernel/cpu/irq.c
+++ source/kernel/cpu/irq.c
@@ -2,7 +2,7 @@
  * @Author: warrior
  * @Date: 2023-07-15 09:50:54
  * @LastEditors: warrior
- * @LastEditTime: 2023-07-15 14:26:59
+ * @LastEditTime: 2023-07-15 16:10:06
  * @Description:
  */
 #include "cpu/irq.h"
@@ -23,7 +23,18 @@ void irq_init(void) {
     for (int i = 0; i < IDT_TABLE_NR; i++) {
         gate_desc_set(idt_table + i, KERNEL_SELECTOR_CS, (uint32_t)exception_handler_unknown, GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_IDT);
     }
+
+    irq_install(IRQ0_DE, (irq_handler_t)exception_handler_divider);
+
     lidt((uint32_t)idt_table, sizeof(idt_table));
+}
+
+int irq_install(int irq_num, irq_handler_t handler) {
+    if (irq_num >= IDT_TABLE_NR) {
+        return -1;
+    }
+    gate_desc_set(idt_table + irq_num, KERNEL_SELECTOR_CS, (uint32_t)handler, GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_IDT);
+    return 0;
 }
 
 /**
@@ -38,5 +49,9 @@ static void do_default_handler(exception_frame_t* frame, const char* message) {
 }
 
 void do_handler_unknown(exception_frame_t* frame) {
-    do_default_handler(frame, "unknown exception");
+    do_default_handler(frame, "Unknown Exception");
+}
+
+void do_handler_divider(exception_frame_t* frame) {
+    do_default_handler(frame, "Divider Exception");
 }
--- source/kernel/include/cpu/irq.h
+++ source/kernel/include/cpu/irq.h
@@ -2,24 +2,40 @@
  * @Author: warrior
  * @Date: 2023-07-15 09:49:17
  * @LastEditors: warrior
- * @LastEditTime: 2023-07-15 14:35:39
+ * @LastEditTime: 2023-07-15 16:07:03
  * @Description: 中断处理相关
  */
 #ifndef IRQ_H
 #define IRQ_H
+
+#define IRQ0_DE 0
 
 #include "comm/types.h"
 
 typedef struct _exception_frame_t {
     uint32_t gs, fs, es, ds;
     uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    uint32_t num, error_code;
     uint32_t eip, cs, eflags;
 } exception_frame_t;
+
+/**
+ * @description: 定义了一个函数指针类型 irq_handler_t,
+ * 指向一个参数为 exception_frame_t* 类型的函数
+ * 这意味着该函数类型可以用于处理异常
+ * @param {exception_frame_t*} frame exception_frame_t
+ * @return {*}
+ */
+typedef void (*irq_handler_t)(exception_frame_t* frame);
 
 /**
  * @description: 初始化中断向量表
  * @return {void}
  */
 void irq_init(void);
+
+void exception_handler_divider(void);
+
+int irq_install(int irq_num, irq_handler_t handler);
 
 #endif
