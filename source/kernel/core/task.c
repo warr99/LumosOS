1d8137f7567ba5a4c1c7ffc21c0155f2922103d5
warrior <warrior22@qq.com>
2023-07-18 17:55:37 +0800

    使用栈进行更高效的任务切换

--- source/kernel/core/task.c
+++ source/kernel/core/task.c
@@ -1,3 +1,10 @@
+/*
+ * @Author: warrior
+ * @Date: 2023-07-18 10:36:04
+ * @LastEditors: warrior
+ * @LastEditTime: 2023-07-18 17:36:53
+ * @Description:
+ */
 #include "core/task.h"
 #include "cpu/cpu.h"
 #include "os_cfg.h"
@@ -26,9 +33,22 @@ static int tss_init(task_t* task, uint32_t entry, uint32_t esp) {
 int task_init(task_t* task, uint32_t entry, uint32_t esp) {
     ASSERT(task != (task_t*)0);
 
-    tss_init(task, entry, esp);
+    // tss_init(task, entry, esp);
+    uint32_t* pesp = (uint32_t*)esp;
+    if (pesp) {
+        *(--pesp) = entry;
+        *(--pesp) = 0;
+        *(--pesp) = 0;
+        *(--pesp) = 0;
+        *(--pesp) = 0;
+        task->stack=pesp;
+    }
+    return 0;
 }
 
+void simple_switch(uint32_t** from, uint32_t* to);
+
 void task_switch(task_t* from, task_t* to) {
-    switch_to_tss(to->tss_sel);
+    // switch_to_tss(to->tss_sel);
+     simple_switch(&from->stack, to->stack);
 }
--- source/kernel/include/core/task.h
+++ source/kernel/include/core/task.h
@@ -4,8 +4,8 @@
 #include "comm/types.h"
 #include "cpu/cpu.h"
 
-
 typedef struct _task_t {
+    uint32_t* stack;
     tss_t tss;
     int tss_sel;
 } task_t;
@@ -13,4 +13,5 @@ typedef struct _task_t {
 int task_init(task_t* task, uint32_t entry, uint32_t esp);
 
 void task_switch(task_t* from, task_t* to);
+
 #endif
