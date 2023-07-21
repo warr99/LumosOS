e028645a755cb71395d1eee42664af2955b79aba
warrior <warrior22@qq.com>
2023-07-21 23:21:21 +0800

    feat(bitmap):位图中位的分配等相关函数的实现

--- source/kernel/include/tools/bitmap.h
+++ source/kernel/include/tools/bitmap.h
@@ -2,8 +2,8 @@
  * @Author: warrior
  * @Date: 2023-07-21 21:35:16
  * @LastEditors: warrior
- * @LastEditTime: 2023-07-21 21:47:20
- * @Description: 
+ * @LastEditTime: 2023-07-21 22:01:59
+ * @Description:
  */
 #ifndef BITMAP_H
 #define BITMAP_H
@@ -31,5 +31,40 @@ int bitmap_byte_count(int bit_count);
  * @return {*}
  */
 void bitmap_init(bitmap_t* bitmap, uint8_t* bits, int count, int init_bit);
+
+/**
+ * @brief: 获取某些位的值
+ * @param {bitmap_t*} bitmap 位图
+ * @param {int} index 索引
+ * @return {int} 值
+ */
+int bitmap_get_bit(bitmap_t* bitmap, int index);
+
+/**
+ * @brief: 设置某些位的值
+ * @param {bitmap_t*} bitmap 位图
+ * @param {int} index 索引
+ * @param {int} count 设置的数量
+ * @param {int} bit 要设置的值
+ * @return {*}
+ */
+void bitmap_set_bit(bitmap_t* bitmap, int index, int count, int bit);
+
+/**
+ * @brief: 判断 index 对应的位是 0 还是 1
+ * @param {bitmap_t*} bitmap 位图
+ * @param {int} index 索引
+ * @return {int} 0 or 1
+ */
+int bit_map_is_set(bitmap_t* bitmap, int index);
+
+/**
+ * @brief: 分配内存中连续为 1 (或者0) 的位
+ * @param {bitmap_t*} bitmap 位图
+ * @param {int} bit 要找 0 还是 1
+ * @param {int} count 数量
+ * @return {*}
+ */
+int bitmap_alloc_nbits(bitmap_t* bitmap, int bit, int count);
 
 #endif
--- source/kernel/tools/bitmap.c
+++ source/kernel/tools/bitmap.c
@@ -11,3 +11,46 @@ void bitmap_init(bitmap_t* bitmap, uint8_t* bits, int count, int init_bit) {
     int bytes = bitmap_byte_count(bitmap->bit_count);
     kernel_memset(bitmap->bits, init_bit ? 0xFF : 0, bytes);
 }
+
+int bitmap_get_bit(bitmap_t* bitmap, int index) {
+    // TODO: return bitmap->bits[index / 8] & (1 << (8 - (index % 8)));
+    return bitmap->bits[index / 8] & (1 << (index % 8)) ? 1 : 0;
+}
+
+void bitmap_set_bit(bitmap_t* bitmap, int index, int count, int bit) {
+    for (int i = 0; (i < count) && (index < bitmap->bit_count); i++, index++) {
+        if (bit) {
+            bitmap->bits[index / 8] |= (1 << (index % 8));
+        } else {
+            bitmap->bits[index / 8] &= ~(1 << (index % 8));
+        }
+    }
+}
+
+int bit_map_is_set(bitmap_t* bitmap, int index) {
+    return bitmap_get_bit(bitmap, index) ? 1 : 0;
+}
+
+int bitmap_alloc_nbits(bitmap_t* bitmap, int bit, int count) {
+    int search_index = 0;
+    int ok_index = -1;
+    int i = 1;
+    while (search_index < bitmap->bit_count) {
+        if (bitmap_get_bit(bitmap, search_index != bit)) {
+            search_index++;
+            continue;
+        }
+        ok_index = search_index;
+        for (i = 1; i < count && (search_index < bitmap->bit_count); i++) {
+            if (bitmap_get_bit(bitmap, search_index++) != bit) {
+                ok_index = -1;
+                break;
+            }
+        }
+        if (i >= count) {
+            bitmap_set_bit(bitmap, ok_index, count, ~bit);
+            return ok_index;
+        }
+    }
+    return -1;
+}
